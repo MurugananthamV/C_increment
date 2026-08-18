7df6b089eb23b7288259bfb3a854cdccba317e21
Murugananthamv <murugananthamv2005@gmail.com>
2026-08-18 20:40:40 +0530

    today i learn input and output in c++

--- C_increment.cpp
+++ C_increment.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+int main(){
+    std::cout<<"enter two number you wan to calculate:"<<std::endl;
+    int a=0,b=0;
+    std::cin>>a>>b;
+    std::cout<<"the sum of two numbe is:"<<a+b<<std::endl;
+}
+    
 
