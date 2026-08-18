1962766241a61d356a95e4edf969f508ec3fbbb4
Murugananthamv <murugananthamv2005@gmail.com>
2026-08-18 21:11:45 +0530

    today i am learn comments

--- C_increment.cpp
+++ C_increment.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+using namespace std;
 int main(){
-    std::cout<<"enter two number you wan to calculate:"<<std::endl;
-    int a=0,b=0;
-    std::cin>>a>>b;
-    std::cout<<"the sum of two numbe is:"<<a+b<<std::endl;
+    // std::cout<<"enter two number you wan to calculate:"<<std::endl;
+    // int a=0,b=0;
+    // std::cin>>a>>b;
+    // std::cout<<"the sum of two numbers is:"<<a+b<<std::endl;
+    std::cout<</*"enter two number you wan to calculate:"*/"k";
+
 }
     
 
